f0f2c74b24c901613cc48916f4ff10d1e0186e42
romulojjunior <romulojjunior@gmail.com>
2020-07-14 19:18:31 -0400

    Added Spinner to select current player.

--- app/src/main/java/com/company/tictacapp/MainActivity.kt
+++ app/src/main/java/com/company/tictacapp/MainActivity.kt
@@ -4,12 +4,19 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import com.company.tictacapp.common.ImageHelper
+import com.company.tictacapp.common.models.Player
+import com.company.tictacapp.common.models.PlayerType
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
+    lateinit var players : List<String>
+    var currentPlayer: Player? = null
 
     companion object {
         const val TAG = "TicTacApp"
@@ -20,8 +27,36 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        loadPlayerList()
+        loadScreenshotButton()
+        loadPlayerSpinner()
+    }
+
+    private fun loadPlayerList() {
+        players = listOf(getString(R.string.choose_your_player), "Player X", "Player O")
+    }
+
+    private fun loadScreenshotButton() {
         screenshotButton?.setOnClickListener {
             openGallery()
+        }
+    }
+
+    private fun loadPlayerSpinner() {
+        val layoutId = android.R.layout.simple_list_item_1
+        val itemsAdapter: ArrayAdapter<String> =  ArrayAdapter<String>(this, layoutId, players)
+
+        playerSpinner.adapter = itemsAdapter
+        playerSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?,  position: Int,  id: Long) {
+                currentPlayer = when (position) {
+                    1 -> Player(type = PlayerType.x)
+                    2 -> Player(type = PlayerType.x)
+                    else -> null
+                }
+                Log.d(TAG, "Current player: ${players[position]}")
+            }
         }
     }
 
